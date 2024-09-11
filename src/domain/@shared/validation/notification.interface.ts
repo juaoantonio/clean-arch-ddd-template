d64e454f26cb080123437749a7f2cdbeec756aba
João Barbosa <joaobarbosadev@gmail.com>
2024-09-11 11:09:56 -0300

    refactor: add notification errors in interface

--- src/domain/@shared/validation/notification.interface.ts
+++ src/domain/@shared/validation/notification.interface.ts
@@ -1,4 +1,5 @@
 export interface INotification {
+  errors: Map<string, string | string[]>;
   addError(error: string, field?: string): void;
   setError(error: string | string[], field?: string): void;
   hasErrors(): boolean;
